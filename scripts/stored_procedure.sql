e52d7e28c3aa129200e6ccb6ee8046d7ada098b1
sidreiG0625 <gacusgideonjunn@gmail.com>
2026-08-11 07:28:19 +0800

    Create stored_procedure.sql

---
+++ scripts/stored_procedure.sql
@@ -0,0 +1,96 @@
+
+/*===================================================================================
+						      - STORE PROCEDURE -
+=====================================================================================*/
+-- STEP 1: Write a query
+-- For US Customers, find the total number of customers and the average scores
+
+ SELECT
+	COUNT(CustomerID) TotalCustomers,
+	AVG(Score) AvgScores
+ FROM Sales.Customers
+ WHERE Country = 'USA'
+
+ -- STEP 2: Turning the query into a stored procedure
+ IF OBJECT_ID('GetCustomerSummary', 'U') IS NOT NULL
+	DROP PROCEDURE GetCustomerSummary;
+GO
+
+-- STORED PROCEDURES: Parameters
+ ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA' AS
+
+ BEGIN
+BEGIN TRY	
+	DECLARE @TotalCustomers INT, @AvgScores FLOAT
+	-- Prepare and Clean the data
+	IF EXISTS(SELECT 1 FROM Sales.Customers	WHERE Score IS NULL AND Country = @Country)
+		BEGIN
+			PRINT ('Updating NULL values to 0');
+			UPDATE Sales.Customers
+			SET Score = 0
+			WHERE Score IS NULL AND Country = @Country;
+		END
+	ELSE
+		BEGIN
+			PRINT ('No NULL values')
+		END;
+
+	-- Generating reports
+	SELECT
+		@TotalCustomers = COUNT(*),
+		@AvgScores = AVG(Score)
+	FROM Sales.Customers
+ WHERE Country = @Country;
+
+ PRINT 'Total customer from ' + @Country + ':' + ' ' + CAST(@TotalCustomers AS NVARCHAR);
+ PRINT 'Average score from ' + @Country + ':' + ' ' + CAST(@AvgScores AS NVARCHAR);
+
+	SELECT
+		COUNT(OrderID) TotalOrders,
+		SUM(Sales) TotalSales
+	FROM Sales.Orders o
+	LEFT JOIN Sales.Customers c
+	ON o.CustomerID = c.CustomerID
+	WHERE c.Country = @Country
+
+END TRY
+BEGIN CATCH
+	PRINT('An error occured!');
+	PRINT('Error Message: ' + ERROR_MESSAGE());
+	PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR));
+	PRINT('Error Procedure: ' + ERROR_PROCEDURE())
+END CATCH
+END
+
+
+
+ -- STEP 3: Execute the stored procedure
+EXEC GetCustomerSummary @Country = 'USA'
+
+SELECT * FROM Sales.Customers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
