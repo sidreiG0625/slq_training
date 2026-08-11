1d5f5f27bfc20332cd90f6681dc2bf024b37b476
sidreiG0625 <gacusgideonjunn@gmail.com>
2026-08-11 15:42:39 +0800

    Update stored_procedure.sql

--- scripts/stored_procedure.sql
+++ scripts/stored_procedure.sql
@@ -1,8 +1,11 @@
-
-/*===================================================================================
+/* ===================================================================================
 						      - STORE PROCEDURE -
-=====================================================================================*/
+===================================================================================== */
+
+
+-- =============================================================================
 -- STEP 1: Write a query
+-- ============================================================================
 -- For US Customers, find the total number of customers and the average scores
 
  SELECT
@@ -16,81 +19,68 @@
 	DROP PROCEDURE GetCustomerSummary;
 GO
 
--- STORED PROCEDURES: Parameters
+-- STORED PROCEDURES: Using Parameters
  ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA' AS
 
- BEGIN
-BEGIN TRY	
-	DECLARE @TotalCustomers INT, @AvgScores FLOAT
-	-- Prepare and Clean the data
-	IF EXISTS(SELECT 1 FROM Sales.Customers	WHERE Score IS NULL AND Country = @Country)
-		BEGIN
-			PRINT ('Updating NULL values to 0');
-			UPDATE Sales.Customers
-			SET Score = 0
-			WHERE Score IS NULL AND Country = @Country;
-		END
-	ELSE
-		BEGIN
-			PRINT ('No NULL values')
-		END;
+BEGIN
+	BEGIN TRY	
+		DECLARE @TotalCustomers INT, @AvgScores FLOAT
 
-	-- Generating reports
-	SELECT
-		@TotalCustomers = COUNT(*),
-		@AvgScores = AVG(Score)
-	FROM Sales.Customers
- WHERE Country = @Country;
+		-- =================================================
+		-- Step 3: Prepare and Clean the data
+		-- =================================================
+		IF EXISTS(SELECT 1 FROM Sales.Customers	WHERE Score IS NULL AND Country = @Country)
+			BEGIN
+				PRINT ('Updating NULL values to 0');
+				UPDATE Sales.Customers
+				SET Score = 0
+				WHERE Score IS NULL AND Country = @Country;
+			END
+		ELSE
+			BEGIN
+				PRINT ('No NULL values')
+			END;
+	
+		-- ==================================================
+		-- Step 4: Generating reports
+		-- ==================================================
+		-- Calculate the Total Customers and Average Score for a specifi country
+		SELECT
+			@TotalCustomers = COUNT(*),
+			@AvgScores = AVG(Score)
 
- PRINT 'Total customer from ' + @Country + ':' + ' ' + CAST(@TotalCustomers AS NVARCHAR);
- PRINT 'Average score from ' + @Country + ':' + ' ' + CAST(@AvgScores AS NVARCHAR);
+		FROM Sales.Customers
+	 WHERE Country = @Country;
 
-	SELECT
-		COUNT(OrderID) TotalOrders,
-		SUM(Sales) TotalSales
-	FROM Sales.Orders o
-	LEFT JOIN Sales.Customers c
-	ON o.CustomerID = c.CustomerID
-	WHERE c.Country = @Country
+	 PRINT 'Total customer from ' + @Country + ':' + ' ' + CAST(@TotalCustomers AS NVARCHAR);
+	 PRINT 'Average score from ' + @Country + ':' + ' ' + CAST(@AvgScores AS NVARCHAR);
 
-END TRY
-BEGIN CATCH
-	PRINT('An error occured!');
-	PRINT('Error Message: ' + ERROR_MESSAGE());
-	PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR));
-	PRINT('Error Procedure: ' + ERROR_PROCEDURE())
-END CATCH
+		-- Calculate the total number of orders and tola sales for a specific country
+		SELECT
+			COUNT(OrderID) TotalOrders,
+			SUM(Sales) TotalSales,
+			1/0
+		FROM Sales.Orders o
+		LEFT JOIN Sales.Customers c
+		ON o.CustomerID = c.CustomerID
+		WHERE c.Country = @Country
+
+	END TRY
+	BEGIN CATCH
+	    -- ====================================================
+		-- ERROR HANDLING
+		-- ====================================================
+		PRINT('An error occured!');
+		PRINT('Error Message: ' + ERROR_MESSAGE());
+		PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR));
+		PRINT('Error Procedure: ' + ERROR_PROCEDURE())
+	END CATCH
 END
 
 
 
- -- STEP 3: Execute the stored procedure
-EXEC GetCustomerSummary @Country = 'USA'
+-- STEP 5: Execute the stored procedures
+EXEC GetCustomerSummary
+EXEC GetCustomerSummary @Country = 'Germany'
 
 SELECT * FROM Sales.Customers
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
