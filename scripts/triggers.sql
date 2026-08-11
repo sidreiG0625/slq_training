973dad988e285f26e8ec2dbabae7130d60629cb7
sidreiG0625 <gacusgideonjunn@gmail.com>
2026-08-11 17:56:53 +0800

    Refactor EmployeeLogs table and update trigger
    Updated EmployeeLogs table structure and modified trigger to log new employee additions correctly.

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -5,48 +5,38 @@
 
 -- USE CASE 1: Maintaining the logs
 -- STEP 1: Create Logs table
-IF OBJECT_ID('EmployeeLogs', 'U') IS NOT NULL
-	DROP TABLE Sales.EmployeeLogs;
-GO
-
 CREATE TABLE Sales.EmployeeLogs (
-	LogID INT NOT NULL,
+	LogID INT IDENTITY(1,1) PRIMARY KEY,
 	EmployeeID INT,
 	LogMessage NVARCHAR(255),
-	LogDate DATETIME,
-	CONSTRAINT p_key PRIMARY KEY (LogID)
-);
+	LogDate DATETIME
+)
 
--- Drop the trigger if it already exists
---IF OBJECT_ID('trg_AfterInsertEmployee', 'TR') IS NOT NULL
-IF EXISTS(SELECT 1 FROM sys.triggers WHERE name = trg_AfterInsertEmployee AND parent_id = OBJECT_ID(Sales.Employees))
-	DROP TRIGGER trg_AfterInsertEmployee;
-GO
-
--- Create new trigger
+--SELECT * FROM Sales.EmployeeLogs 
+-- STEP 2: Create the trigger
 CREATE TRIGGER trg_AfterInsertEmployee ON Sales.Employees
-AFTER INSERT
+AFTER INSERT 
 AS
 BEGIN
-	INSERT INTO EmployeeLogs (EmployeeID, LogMessage, LogDate)
-	SELECT
+	INSERT INTO Sales.EmployeeLogs(EmployeeID, LogMessage, LogDate)
+	SELECT 
 		EmployeeID,
-		'New Employee Added: ' + CAST(EmployeeID AS NVARCHAR),
+		'New employee has been added: ' + CAST(EmployeeID AS NVARCHAR),
 		GETDATE()
 	FROM INSERTED;
-END;
-
-SELECT * FROM Sales.Employees
-SELECT * FROM EmployeeLogs
-
-DELETE Sales.Employees
-WHERE EmployeeID = 6
-
+END
 
 
 INSERT INTO Sales.Employees 
 VALUES
-(6, 'Sofia', 'Gacus', 'Engineering', '2016-11-03', 'F', 20000, 2)
+(1, 'Sidrei', 'Gacus', 'Information Systes', '2025-06-08', 'M', 250000, 3),
+
+
+SELECT * FROM Sales.Employees
+SELECT * FROM Sales.EmployeeLogs 
+
+DELETE SAles.Employees
+WHERE EmployeeID = 1
 
 
 
