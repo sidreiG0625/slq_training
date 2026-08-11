daf1f9ad7d663c3fd8649eac5bbe905a4f7148a2
sidreiG0625 <gacusgideonjunn@gmail.com>
2026-08-11 18:05:00 -0400

    Update performance_optimization.sql

--- scripts/performance_optimization.sql
+++ scripts/performance_optimization.sql
@@ -6,25 +6,49 @@
                           -- 3. Performance Tips (BEST PRACTICES)
                                 -- Always check the execution plan to confirm performance improvement
                                    when optimizing query. If theres no improvement, focus on readibility
-                                -- BEST PRACTICES FOR SELECT STATEMENTS *
+                              -- BEST PRACTICES FOR SELECT STATEMENTS *
                                   -- Select only the columns you need
                                   -- Avoid unnecessary DISTINCT and ORDER BY
                                   -- For exploration purposes, limit the rows
 
-                                -- BEST PRACTICES FOR FILTERING * 
+                              -- BEST PRACTICES FOR FILTERING * 
                                   -- Create a nonclustered index on frequently used columns in where clause
                                   -- Avoid applying functions to columns in WHERE clause
                                   -- Avoid using leading wildcard as they prevent index usage
                                   -- Use IN instead of multiple OR conditions
 
-                                -- BEST PRACTICES FOR JOINING STATEMENTS * 
+                              -- BEST PRACTICES FOR JOINING STATEMENTS * 
                                   -- Understand the speed of joins & Use of INNER JOIN when possible
                                   -- Use explicit joins (ANSI join) instead of implicit joins (non-ANSI Join)
                                   -- Make sure to index the columns used in the ON clause
                                   -- Filter before joining tables
                                   -- Aggregate data before joining tables
                                   -- Use UNION instead of OR in joins
+                                  -- Use UNION ALL instead of UNTION if duplicates are acceptable
+                                  -- Use UNION ALL and DISTINCT instead of using UNION if duplicates are not acceptable
                                   -- Check for Nested loops and use SQL hints
+
+                              -- BEST PRACTICES FOR AGGREGATIONS * 
+                                  -- Use columnstore index for aggregations in large tables
+                                  -- Pre-aggregate the data and store it in a new table for reporting
+
+                              -- BEST PRACTICES FOR SUBQUERIES * 
+                                  -- Use EXISTS for large tables than JOINS (small tables) on subquery
+                                  -- Avoid redundant logic in your query
+
+                              -- BEST PRACTICES FOR CREATING TABLES (DDL) * 
+                                  -- Avoid using VARCHAR and TEXT datatypes
+                                  -- Avoid using MAX unnecessarily large lengths in data types
+                                  -- Use the NOT NULL constraint where applicable
+                                  -- Ensure all your tables have a clustered primary key
+                                  -- Create a non-clustered index for foreign keys that are frequently used
+
+                              -- BEST PRACTICES FOR INDEXING/PARTITIONING *
+                                 -- Avoid overindexing
+                                 -- drop unused indexing
+                                 -- update index statistics (weekly)
+                                 -- reorganize or rebuild indexes
+                                 -- partition largem fact tables to improve performance
 
 -- METHOD 1. INDEX MANAGEMENT --
 -- Monitor Index usage. Drop unused indexes
